21af3e62448ca03b65cba846ba45110fd4d4652a
NghiaPham293/ASM <nghia29032001@gmail.com>
2023-07-11 19:32:00 +0700

    //nghia

--- app/src/main/java/com/example/duan1_quanlysalon/DetailBookingActivity.java
+++ app/src/main/java/com/example/duan1_quanlysalon/DetailBookingActivity.java
@@ -10,5 +10,6 @@ public class DetailBookingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_booking);
+        //nghia
     }
 }
