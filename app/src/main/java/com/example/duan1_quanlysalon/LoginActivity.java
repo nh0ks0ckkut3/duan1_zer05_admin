2a7cca392b9eb591e46f5922c5fc0fbdf4fda048
van hoang <hoanglvps26283@fpt.edu.vn>
2023-07-12 22:31:44 +0700

    check_hide/show

--- app/src/main/java/com/example/duan1_quanlysalon/LoginActivity.java
+++ app/src/main/java/com/example/duan1_quanlysalon/LoginActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.duan1_quanlysalon.database.EmployeeDAO;
@@ -17,7 +20,9 @@ import com.example.duan1_quanlysalon.model.Employee;
 public class LoginActivity extends AppCompatActivity {
     EditText edtUserName, edtPassWord;
     CheckBox chkRemember;
+    ImageButton icon_pass;
     SharedPreferences sharedPreferences;
+
 
     String user, pass;
     EmployeeDAO employeeDAO;
@@ -29,8 +34,26 @@ public class LoginActivity extends AppCompatActivity {
         Button btn_login = findViewById(R.id.btn_login);
         edtUserName = findViewById(R.id.edtUserName);
         edtPassWord = findViewById(R.id.edtPassWord);
+        icon_pass = findViewById(R.id.icon_pass);
         chkRemember = findViewById(R.id.chkRemember);
         employeeDAO = new EmployeeDAO(this);
+
+        icon_pass.setBackgroundResource(R.drawable.hidepass);
+        icon_pass.setOnClickListener(new View.OnClickListener() {
+            boolean hidePass = false;
+            @Override
+            public void onClick(View v) {
+                if (hidePass){
+                    edtPassWord.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD | InputType.TYPE_CLASS_TEXT);
+                    icon_pass.setBackgroundResource(R.drawable.showpass);
+                }else{
+                    edtPassWord.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    icon_pass.setBackgroundResource(R.drawable.hidepass);
+                }
+                    hidePass =! hidePass;
+                edtPassWord.setSelection(edtPassWord.getText().length());
+            }
+        });
 
         sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
         boolean check = sharedPreferences.getBoolean("isRemember", false);
