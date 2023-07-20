2e9273185c3f5912f5ead714d23a601a45660643
nh0ks0ckkut3 <110466193+nh0ks0ckkut3@users.noreply.github.com>
2023-07-20 11:25:37 +0700

    Update Menu_Fragment.java

--- app/src/main/java/com/example/duan1_quanlysalon/fragment/Menu_Fragment.java
+++ app/src/main/java/com/example/duan1_quanlysalon/fragment/Menu_Fragment.java
@@ -3,17 +3,6 @@ package com.example.duan1_quanlysalon.fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-<<<<<<< HEAD
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
-import androidx.cardview.widget.CardView;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-=======
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,16 +17,12 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
->>>>>>> 569aeb37634b280c4e9a111ee82332a2c6f59cdc
 
 import com.example.duan1_quanlysalon.DSdichvuActivity;
 import com.example.duan1_quanlysalon.DSnhanvienActivity;
 import com.example.duan1_quanlysalon.DSsanphamActivity;
 import com.example.duan1_quanlysalon.R;
-<<<<<<< HEAD
-=======
 import com.google.android.material.navigation.NavigationView;
->>>>>>> 569aeb37634b280c4e9a111ee82332a2c6f59cdc
 
 public class Menu_Fragment extends Fragment {
     DrawerLayout drawerLayout;
