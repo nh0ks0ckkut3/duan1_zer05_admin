9b82c97be32c17db7c5cddcec71b1596d8a06281
nh0ks0ckkut3 <110466193+nh0ks0ckkut3@users.noreply.github.com>
2023-07-20 11:26:44 +0700

    Update DSnhanvienActivity.java

--- app/src/main/java/com/example/duan1_quanlysalon/DSnhanvienActivity.java
+++ app/src/main/java/com/example/duan1_quanlysalon/DSnhanvienActivity.java
@@ -1,13 +1,7 @@
 package com.example.duan1_quanlysalon;
 
-<<<<<<< HEAD
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-=======
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
->>>>>>> 569aeb37634b280c4e9a111ee82332a2c6f59cdc
 
 public class DSnhanvienActivity extends AppCompatActivity {
 
